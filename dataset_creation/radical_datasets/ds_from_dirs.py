e1c753f1cd610512d17c4f0e7aa15b61d2e85e95
Leif <leif.seute@gmx.de>
2023-12-18 20:34:18 +0100

    scripts for erics datasets

--- dataset_creation/radical_datasets/ds_from_dirs.py
+++ dataset_creation/radical_datasets/ds_from_dirs.py
@@ -1,30 +1,44 @@
 """
-ASSUME THAT GRADIENTS ARE STORED WITH WRONG SIGN!
 Assuming a dataset is stored as folder/molname/moldata.npz, this script creates a directory contianing molname.npz files.
 """
 
 from pathlib import Path
 from grappa.data import MolData
+from grappa.utils import openmm_utils
+import shutil
+from typing import List
 
-def main(sourcepath, targetpath):
+def main(sourcepath, targetpath, openmm_ff:str=None, skip:List[str]=[]):
+    if not openmm_ff is None:
+        openmm_ff_name = openmm_ff.strip('.xml')
+        openmm_ff_name = openmm_ff_name.strip('*')
+        openmm_ff = openmm_utils.get_openmm_forcefield(openmm_ff)
+
     sourcepath = Path(sourcepath)
     targetpath = Path(targetpath)
     targetpath.mkdir(parents=True, exist_ok=True)
-    for moldir in sourcepath.iterdir():
+    for i,moldir in enumerate(sourcepath.iterdir()):
         if not moldir.is_dir():
+            # copy file to target:
+            shutil.copy(moldir, targetpath)
             continue
-        molname = moldir.name
-        moldata = MolData.load(moldir/'moldata.npz')
-        
-        ###############################
-        # reverse gradients:
-        moldata.gradient = -moldata.gradient
-        moldata.gradient_ref = -moldata.gradient_ref
-        for v in moldata.ff_gradient.values():
-            v *= -1
-        for v in moldata.ff_nonbonded_gradient.values():
-            v *= -1
-        ###############################
+
+        print(f"Processing {i}, {moldir.name}        ", end='\r')
+        try:
+            molname = moldir.name
+            moldata = MolData.load(moldir/'moldata.npz')
+            if moldata.xyz.shape[0] == 0:
+                print(f"Skipping {molname} because it has no conformers.")
+                continue
+
+            if moldata.pdb is not None and openmm_ff is not None:
+                if molname in skip:
+                    print(f"Skipping energy calculation of {molname}")
+                else:
+                    moldata.calc_energies_openmm(openmm_forcefield=openmm_ff, forcefield_name=openmm_ff_name)
+        except:
+            print() # for not overwriting the previous line
+            raise
 
         moldata.save(targetpath/molname)
 
@@ -33,5 +47,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--source_path', type=str, help='Path to the folder with npz files defining MolData objects.')
     parser.add_argument('--target_path', type=str, help='Path to the folder where the new MolData objects should be stored.')
+    parser.add_argument('--openmm_ff', type=str, default=None, help='Path to the openmm forcefield xml file. If given, the energies and gradients are calculated using openmm. Can be eg "amber99sbildn.xml".')
+    parser.add_argument('--skip', type=str, default=[], nargs='+', help='List of molnames to skip.')
     args = parser.parse_args()
-    main(args.source_path, args.target_path)
+    main(args.source_path, args.target_path, args.openmm_ff, skip=args.skip)
---
+++ dataset_creation/radical_datasets/erics_datasets.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e # exit on first error
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )" # dir in which this script lies
+
+source_path="$SCRIPT_DIR/../../../../hartmaec/workdir/new_dataset/dataset_clean"
+target_path="$SCRIPT_DIR/../../data/grappa_datasets"
+dglpath="$SCRIPT_DIR/../../data/dgl_datasets"
+
+
+# List of dataset names
+datasets=("AA_natural" "AA_radical")
+
+target_ds_names=("AA_natural" "AA_radical")
+
+# Loop through each dataset name
+for i in "${!datasets[@]}"; do
+    ds="${datasets[$i]}"
+    target_ds_name="${target_ds_names[$i]}"
+    forcefield="${forcefields[$i]}"
+    forcefield_type="${forcefield_types[$i]}"
+    echo "Processing $ds"
+
+    # for AA_natural, parametrize with a forcefield, else
+    if [ "$ds" == "AA_natural" ]; then
+        python ds_from_dirs.py --source_path "$source_path/$ds" --target_path "$target_path/$target_ds_name" --openmm_ff "amber99sbildn*" --skip J_zeta Y_deprotonated J_epsilon
+    else
+        python ds_from_dirs.py --source_path "$source_path/$ds" --target_path "$target_path/$target_ds_name"
+    fi
+
+    # Convert to dgl dataset
+    python ../benchmark_datasets/to_dgl.py --source_path "$target_path/$target_ds_name" --target_path "$dglpath/$target_ds_name"
+
+done
