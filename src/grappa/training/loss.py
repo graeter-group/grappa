9d5f34a1b692f5abd6fb2b2e178e7aa5dc15b36b
Leif <leif.seute@gmx.de>
2023-12-28 17:14:02 +0100

    modified loss function for batching of all confs

--- src/grappa/training/loss.py
+++ src/grappa/training/loss.py
@@ -5,7 +5,7 @@ A collection of loss functions for training grappa models.
 import dgl
 import torch
 from typing import Dict, List
-from grappa.utils import graph_utils
+from grappa.utils import graph_utils, dgl_utils
 
 
 class MolwiseLoss(torch.nn.Module):
@@ -15,9 +15,10 @@ class MolwiseLoss(torch.nn.Module):
     """
     def __init__(
         self,
-        gradient_weight:float=10.0,
+        gradient_weight:float=0.5,
         energy_weight:float=1.0,
-        param_weight:float=1e-3,
+        param_weight:float=1e-4,
+        tuplewise_weight:float=1e-4,
         weights:Dict[str,float]={"n2_k":1e-3, "n3_k":1e-2},
         skip_params_if_not_present:bool=True,
         proper_regularisation:float=0., # prefactor for L2 regularisation of proper torsion parameters
@@ -29,6 +30,7 @@ class MolwiseLoss(torch.nn.Module):
         self.gradient_weight = gradient_weight
         self.energy_weight = energy_weight
         self.param_weight = param_weight
+        self.tuplewise_weight = tuplewise_weight
         self.weights = weights
         self.skip_params_if_not_present = skip_params_if_not_present
         self.proper_regularisation = proper_regularisation
@@ -38,7 +40,7 @@ class MolwiseLoss(torch.nn.Module):
     def forward(self, g):
 
         loss = 0
-        graphs = dgl.unbatch(g)
+        graphs = dgl_utils.unbatch(g)
 
         assert not (self.gradient_weight == 0 and self.energy_weight == 0 and self.param_weight == 0), "At least one of the weights must be non-zero."
 
@@ -47,19 +49,21 @@ class MolwiseLoss(torch.nn.Module):
             # loss per graph:
             loss_term = 0.
 
-            if self.gradient_weight != 0.:
-                gradients = graph_utils.get_gradients(graph)
-                gradients_ref = graph_utils.get_gradients(graph, suffix="_ref")
-                loss_term = loss_term + self.mse_loss(gradients, gradients_ref) * self.gradient_weight
-
             if self.energy_weight != 0.:
                 energies = graph_utils.get_energies(graph)
                 energies_ref = graph_utils.get_energies(graph, suffix="_ref")
+                assert energies.shape == energies_ref.shape, f"Shape of energies and energies_ref do not match: {energies.shape} vs {energies_ref.shape}"
                 energy_contrib = self.mse_loss(energies, energies_ref) * self.energy_weight
                 loss_term = loss_term + energy_contrib
 
+            if self.gradient_weight != 0.:
+                gradients = graph_utils.get_gradients(graph)
+                gradients_ref = graph_utils.get_gradients(graph, suffix="_ref")
+                assert gradients.shape == gradients_ref.shape, f"Shape of gradients and gradients_ref do not match: {gradients.shape} vs {gradients_ref.shape}"
+                loss_term = loss_term + self.mse_loss(gradients, gradients_ref) * self.gradient_weight
 
-            if self.param_weight != 0:
+
+            if self.param_weight != 0.:
                 try:
                     params_ref = graph_utils.get_parameters(graph, suffix="_ref")
                 except KeyError as e:
@@ -116,6 +120,35 @@ class MolwiseLoss(torch.nn.Module):
                     loss_term = loss_term + self.improper_regularisation * torch.mean(torch.square(impropers))
                 loss_term = loss_term + self.improper_regularisation * torch.mean(torch.square(impropers))
 
+            # if self.tuplewise_weight != 0.:
+            #     tuplewise_energies = graph_utils.get_tuplewise_energies(graph)
+            #     tuplewise_energies_ref = graph_utils.get_tuplewise_energies(graph, suffix="_classical_ff")
+
+            #     pred_energies = []
+            #     ref_energies = []
+
+            #     for k, predicted_energies in tuplewise_energies.items():
+            #         if k not in tuplewise_energies_ref.keys():
+            #             raise ValueError(f"Tuplewise energy {k} not in tuplewise_energies_ref")
+            #         if tuplewise_energies[k].shape != tuplewise_energies_ref[k].shape:
+            #             raise ValueError(f"Shape of tuplewise energy {k} and {k}_ref do not match: {tuplewise_energies[k].shape} vs {tuplewise_energies_ref[k].shape}")
+
+            #         classical_energies = tuplewise_energies_ref[k]
+
+            #         # set all energies to zero where the ref energies are nan. This enables batching graphs with parameters and without
+            #         predicted_energies = torch.where(torch.isnan(classical_energies), torch.zeros_like(predicted_energies), predicted_energies)
+            #         classical_energies = torch.where(torch.isnan(classical_energies), torch.zeros_like(classical_energies), classical_energies)
+
+            #         pred_energies.append(predicted_energies.flatten())
+            #         ref_energies.append(classical_energies.flatten())
+
+            #     pred_energies = torch.cat(pred_energies)
+            #     ref_energies = torch.cat(ref_energies)
+                
+            #     tuplewise_loss_contrib = torch.mean(torch.square(pred_energies - ref_energies)) * self.tuplewise_weight
+
+            #     loss_term = loss_term + tuplewise_loss_contrib
+
             loss = loss + loss_term/len(graphs)
     
         return loss
@@ -135,169 +168,3 @@ def correct_torsion_shape(x, shape1):
         else:
             return x
         
-
-
-
-class ParameterLoss(torch.nn.Module):
-    """
-    Returns the MSE of the parameters of a graph asuming that they are stored at g.nodes[lvl].data[{k}/{eq}] and g.nodes[lvl].data[{k}/{eq}_ref]
-    The SEs of the parameters are weighted by the square of the weight attribute. This is necessary because the units of the parameters are different.
-    """
-    def __init__(
-        self, 
-        weights:Dict[str,float]={"n2_k":1e-3, "n3_k":1e-2},
-        param_names:List[str]=["n2_k", "n2_eq", "n3_k", "n3_eq", "n4_k"],
-        average_params:bool=False,
-        ):
-
-        super().__init__()
-        self.weights = weights
-        self.mse_loss = torch.nn.MSELoss()
-        self.param_names = param_names
-        self.average_params = average_params
-
-
-    def forward(self, g):
-        loss = 0
-        batch_size = g.num_nodes("g")
-        params = graph_utils.get_parameters(g)
-        params_ref = graph_utils.get_parameters(g, suffix="_ref")
-        for param in self.param_names:
-            if self.average_params:
-                loss_term = self.mse_loss(params[param], params_ref[param])
-            else:
-                # divide by the number of batches, sum over the tuple dimension
-                loss_term = torch.square(params[param] - params_ref[param]).sum() / batch_size
-
-            if param in self.weights.keys():
-                loss_term = loss_term * (self.weights[param]**2)
-
-            loss = loss + loss_term
-        
-        return loss
-    
-
-class TuplewiseEnergyLoss(torch.nn.Module):
-    """
-    Assumes that the graph has been passed through grappa.models.Energy(suffix="_ref", write_suffix="_classical_ff", gradents=True)!
-
-    For classical force fields, we can split the energy into contributions from specific interactions. Thus, we can use the individual energy contributions (before pooling!) as targets to obtain a more informative gradient signal.
-    This is comparable to the more informative signal obained by matching gradients instead of energies but here, we not only have a spatial resolution but also a resolution in terms of the interaction term in the energy parameterization.
-    In comparision to the ParameterLoss, this offers the following advantages:
-        - The units of the loss contributions are the same for all parameters, no re-weighting is necessary.
-        - We can learn classical contributions of improper torsion.
-    """
-    def __init__(
-        self, 
-        weights:Dict[str,float]={},
-        interactions:List[str]=["n2", "n2", "n3", "n4", "n4_improper"],
-        average_interactions:bool=False,
-        ):
-
-        super().__init__()
-        self.weights = weights
-        self.mse_loss = torch.nn.MSELoss()
-        self.interactions = interactions
-        self.average_interactions = average_interactions
-
-
-    def forward(self, g):
-        loss = 0
-        batch_size = g.num_nodes("g")
-        energies = graph_utils.get_tuplewise_energies(g)
-        energies_ref = graph_utils.get_tuplewise_energies(g, suffix="_classical_ff")
-        for interaction in self.interactions:
-            if self.average_interactions:
-                loss_term = self.mse_loss(energies[interaction], energies_ref[interaction])
-            else:
-                # divide by the number of batches, sum over the tuple dimension
-                loss_term = torch.square(energies[interaction] - energies_ref[interaction]).sum() / batch_size
-            if interaction in self.weights.keys():
-                loss_term = loss_term * self.weights
-            loss = loss + loss_term
-        
-        return loss
-    
-
-class EnergyLoss(torch.nn.Module):
-    """
-    Use energies as targets. The SEs of energies are averaged over conformation and batch.
-    """
-    def __init__(
-        self, 
-        energy_weight:float=1.0,
-        ):
-
-        super().__init__()
-        self.mse_loss = torch.nn.MSELoss()
-        self.energy_weight = energy_weight
-
-    def forward(self, g):
-        loss = 0
-        energies = graph_utils.get_energies(g)
-        energies_ref = graph_utils.get_energies(g, suffix="_ref")
-
-        if self.energy_weight != 0:
-            # mean over batch and conf
-            loss = loss + self.mse_loss(energies, energies_ref) * self.energy_weight
-
-        return loss
-    
-
-class GradientLoss(torch.nn.Module):
-    """
-    Use gradients as targets. The SEs of gradients are averaged over the spatial dimension, conformation divided by the batch_size and weighted by a hyperparameter, the gradient_weight. If average_forces is set to True, the SEs of the gradients are averaged over the atom dimension as well, which could be problematic because then the loss depends strongly on how the batch is being formed. (If the error is large for a force in a small molecule, this is surpressed if paired with a large molecule in a batch as opposed to another small molecule.)
-    """
-    def __init__(
-        self, 
-        gradient_weight:float=1.0,
-        average_forces:bool=False
-        ):
-
-        super().__init__()
-        self.mse_loss = torch.nn.MSELoss()
-        self.gradient_weight = gradient_weight
-        self.average_forces = average_forces
-
-    def forward(self, g):
-        loss = 0
-        num_batches = g.num_nodes("g")
-        gradients = graph_utils.get_gradients(g)
-        gradients_ref = graph_utils.get_gradients(g, suffix="_ref")
-
-        if self.gradient_weight != 0:
-            squared_diffs = torch.square(gradients - gradients_ref)
-            # shape of squared_diffs: (num_atoms_batch, num_confs, 3)
-            if self.average_forces:
-                # mean over all dimensions (the batch is already included implicity because the nodes are concatenated)
-                loss = loss + torch.mean(squared_diffs) * self.gradient_weight
-            else:
-                # mean over conformation dimension and divide by number of batches (there is no batch dimensions, nodes are concatenated)
-                loss = loss + torch.sum(torch.mean(squared_diffs, dim=1)) * self.gradient_weight / num_batches
-
-        return loss
-    
-
-class GradientLossMolwise(torch.nn.Module):
-    """
-    Unbatch the graph and calculate the mse averaged over spatial dimension, conformation and atom. Then average these over the batch. Note: The averages do not commute because the number of atoms in the graph may differ. This way we assign the same weight to molecules with different amounts of atoms.
-    """
-    def __init__(
-        self, 
-        ):
-
-        super().__init__()
-        self.mse_loss = torch.nn.MSELoss()
-
-    def forward(self, g):
-        loss = 0
-        graphs = dgl.unbatch(g)
-
-        for graph in graphs:
-            gradients = graph_utils.get_gradients(graph)
-            gradients_ref = graph_utils.get_gradients(graph, suffix="_ref")
-
-            loss = loss + self.mse_loss(gradients, gradients_ref)/len(graphs)
-    
-        return loss
-    
