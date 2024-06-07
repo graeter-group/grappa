0b522f9f73ac516883b425a66d11c47108ff1202
Leif Seute <leif.seute@gmx.de>
2024-06-07 14:17:11 +0200

    dataset creation

--- src/grappa/constants.py
+++ src/grappa/constants.py
@@ -46,3 +46,63 @@ MAX_NUM_CHARGE_MODELS = 10 # the dimension used for one-hot encoding of the char
 CHARGE_MODELS = ['am1BCC', 'amber99', 'charmm'] # amber99: amber99ffsbildn-charges for peptides. The idea is that the tag 'amber99' can refer to different charge models for different types of molecules (which grappa can usually distinguish). e.g. one could train for rna on charges from some classical rna forcefield without introducing an additional tag.
 
 BONDED_CONTRIBUTIONS = [("n2","k"), ("n2","eq"), ("n3","k"), ("n3","eq"), ("n4","k"), ("n4_improper","k")]
+
+
+
+
+# creation: see tests/misc/masses.py
+ATOMIC_MASSES = {
+    1: 1.008,
+    2: 4.002,
+    3: 6.94,
+    4: 9.012,
+    5: 10.81,
+    6: 12.011,
+    7: 14.007,
+    8: 15.999,
+    9: 18.998,
+    10: 20.1797,
+    11: 22.989,
+    12: 24.305,
+    13: 26.981,
+    14: 28.085,
+    15: 30.973,
+    16: 32.06,
+    17: 35.45,
+    18: 39.95,
+    19: 39.0983,
+    20: 40.078,
+    21: 44.955,
+    22: 47.867,
+    23: 50.9415,
+    24: 51.9961,
+    25: 54.938,
+    26: 55.845,
+    27: 58.933,
+    28: 58.6934,
+    29: 63.546,
+    30: 65.38,
+    31: 69.723,
+    32: 72.63,
+    33: 74.921,
+    34: 78.971,
+    35: 79.904,
+    36: 83.798,
+    37: 85.4678,
+    38: 87.62,
+    39: 88.905,
+    40: 91.224,
+    41: 92.906,
+    42: 95.95,
+    43: 97.0,
+    44: 101.07,
+    45: 102.905,
+    46: 106.42,
+    47: 107.8682,
+    48: 112.414,
+    49: 114.818,
+    50: 118.71,
+    51: 121.76,
+    52: 127.6,
+    53: 126.904
+}
