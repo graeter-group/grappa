35fd145386e840c3054d4e5ea49c24e6c9a6d13f
Eddi Merbitz <merbitei@cascade-002.cluster>
2025-01-22 16:28:02 +0100

    Add grappa atom as node input feature

--- src/grappa/models/graph_attention.py
+++ src/grappa/models/graph_attention.py
@@ -66,6 +66,7 @@ class GrappaGNN(torch.nn.Module):
                 "degree": 6,
                 "is_radical": 1,
                 "charge_model": len(constants.CHARGE_MODELS)-1, # NOTE: set to MAX_NUMBER_OF_CHARGE_MODELS, DEPRECATED
+                "grappa_atom": 1,
             }
             # overwrite/append to these default values:
             for key in in_feat_dims.keys():
